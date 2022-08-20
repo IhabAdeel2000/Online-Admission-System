96e8c4b20750846bccbdd36130ded506e17c78f6
Adeel Ihab <AdeelIhab@JohnDeere.com>
2022-08-20 15:57:46 +0530

    first commit

---
+++ Online-Admission-System/src/test/java/com/ia/boot/project/Online/Admission/System/OnlineAdmissionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.boot.project.Online.Admission.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAdmissionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
